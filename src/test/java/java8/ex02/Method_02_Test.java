06ca2c62b4dba4dabe00666df9eb4594a4406d98
Kévin SEGUINEAU <kevinseguineau+git@gmail.com>
2019-05-23 11:35:28 +0200

    correction assert

--- src/test/java/java8/ex02/Method_02_Test.java
+++ src/test/java/java8/ex02/Method_02_Test.java
@@ -61,6 +61,6 @@ public class Method_02_Test {
 		// TODO invoquer la méthode format() pour que le test soit passant
 		String result = daoA.format();
 
-		"DaoA[20 persons]".equals(result);
+		assert "DaoA[20 persons]".equals(result);
 	}
 }
